b446f79b04ab8aede1beef59f363349e4e5a3ede
vsydorovskyi <vsydorovskyi@lohika.com>
2023-11-23 02:27:45 +0200

    initial commit

---
+++ src/constants/activityFeedConstants.ts
@@ -0,0 +1,11 @@
+export const activityFeedTypes = [
+  'message', 'phone', 'coffee', 'beer', 'meeting'
+];
+
+export const explanatoryText: { [key: string]: string } = {
+  message: 'added a note to',
+  coffee: 'had a coffee with',
+  meeting: 'had a meeting with',
+  phone: 'had a call with',
+  beer: 'had a beer with'
+};
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,7 @@
+export interface IActivityFeed {
+  id: string;
+  createdAt: string;
+  type: string;
+  explanatoryText: string;
+  note: string;
+}
